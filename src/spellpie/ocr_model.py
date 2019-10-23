15488b8ba913d984bbccaa599cb85557d51c2138
dcronkite <dcronkite@gmail.com>
2019-10-23 08:37:31 -0700

    Keep track of all changes

--- src/spellpie/ocr_model.py
+++ src/spellpie/ocr_model.py
@@ -61,36 +61,43 @@ class Words:
         return self.words[self.curr - 2]
 
 
-def ocr_spell_correct_line(lm: TrigramLanguageModel, line, cutoff=3):
-    newline = []
-    pat = re.compile(r'[A-Za-z]+')
-    words = Words()
-    apparent_word_ctr = 0
-    for m in pat.finditer(line):
-        word = m.group()
-        apparent_word = len(word) > cutoff and word in lm
-        if apparent_word:
-            apparent_word_ctr += 1
-        words.append(Word(word.lower(), m.start(), m.end(), apparent_word, word))
-    idx = 0
-    for word in words:
-        if word.word in lm or len(word.word) <= cutoff:
-            newline.append(line[idx:word.end])
-        else:  # out-of-vocab long word
-            ppw = words.prev_prev_word().word
-            pw = words.prev_word().word
-            nw = words.next_word().word
-            nnw = words.next_next_word().word
-            best_candidate = None
-            best_score = 0
-            for cand, diff in ((word.word, 0), ) + tuple(lm.generate_candidates(word.word)):
-                score = lm.sum((cand,), (pw, cand), (ppw, pw, cand),
-                               (pw, cand, nw), (cand, nw), (cand, nw, nnw))
-                if not best_candidate or score > best_score:
-                    best_candidate = cand
-                    best_score = score
-            newline.append(line[idx:word.start])
-            newline.append(''.join(x if x.lower() == y.lower() else y for x, y in zip(word.orig_word, best_candidate)))
-        idx = word.end
-    newline.append(line[idx:])
-    return ''.join(newline)
+class OcrSpellCorrector:
+
+    def __init__(self):
+        self.changes = []
+
+    def spell_correct_line(self, lm: TrigramLanguageModel, line, cutoff=3):
+        newline = []
+        pat = re.compile(r'[A-Za-z]+')
+        words = Words()
+        apparent_word_ctr = 0
+        for m in pat.finditer(line):
+            word = m.group()
+            apparent_word = len(word) > cutoff and word in lm
+            if apparent_word:
+                apparent_word_ctr += 1
+            words.append(Word(word.lower(), m.start(), m.end(), apparent_word, word))
+        idx = 0
+        for word in words:
+            if word.word in lm or len(word.word) <= cutoff:
+                newline.append(line[idx:word.end])
+            else:  # out-of-vocab long word
+                ppw = words.prev_prev_word().word
+                pw = words.prev_word().word
+                nw = words.next_word().word
+                nnw = words.next_next_word().word
+                best_candidate = None
+                best_score = 0
+                for cand, diff in ((word.word, 0), ) + tuple(lm.generate_candidates(word.word)):
+                    score = lm.sum((cand,), (pw, cand), (ppw, pw, cand),
+                                   (pw, cand, nw), (cand, nw), (cand, nw, nnw))
+                    if not best_candidate or score > best_score:
+                        best_candidate = cand
+                        best_score = score
+                newline.append(line[idx:word.start])
+                newword = ''.join(x if x.lower() == y.lower() else y for x, y in zip(word.orig_word, best_candidate))
+                self.changes.append((word.orig_word, newword, line[word.start - 100:word.end + 100]))
+                newline.append(newword)
+            idx = word.end
+        newline.append(line[idx:])
+        return ''.join(newline)
--- tests/test_ocr.py
+++ tests/test_ocr.py
@@ -1,7 +1,7 @@
 import pytest
 
 from spellpie.lm import build_spelling_model
-from spellpie.ocr_model import ocr_spell_correct_line
+from spellpie.ocr_model import OcrSpellCorrector
 
 
 @pytest.fixture()
@@ -11,16 +11,21 @@ def simple_lm():
     ])
 
 
+@pytest.fixture()
+def ocr_spell_corrector():
+    return OcrSpellCorrector()
+
+
 @pytest.mark.parametrize(('line', ), [
     (r'(ous 0 ==> oes .+ . ale e=', )
 ])
-def test_garbage_pattern(simple_lm, line):
+def test_garbage_pattern(simple_lm, ocr_spell_corrector, line):
     """These patterns should not be altered"""
-    assert ocr_spell_correct_line(simple_lm, line) == line
+    assert ocr_spell_corrector.spell_correct_line(simple_lm, line) == line
 
 
 @pytest.mark.parametrize(('line', 'exp'), [
     (r'immature Granuiocvtes% 0: cl e % BRH', r'immature Granulocytes% 0: cl e % BRH')
 ])
-def test_good_pattern(simple_lm, line, exp):
-    assert ocr_spell_correct_line(simple_lm, line) == exp
+def test_good_pattern(simple_lm, ocr_spell_corrector, line, exp):
+    assert ocr_spell_corrector.spell_correct_line(simple_lm, line) == exp
