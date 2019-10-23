0d5e299620812c6b5eeaaf7ec5b699292de66166
dcronkite <dcronkite@gmail.com>
2019-10-23 08:01:39 -0700

    Added complexity, and getting base test to pass (i.e., no valid words). Now need to start generating candidates.

--- src/spellpie/ocr_model.py
+++ src/spellpie/ocr_model.py
@@ -5,6 +5,75 @@ OCR model. When doing OCR, much of the incoming data is likely
     which appear to lack any content at all (e.g., no in-dictionary
     words).
 """
+import collections
+import re
 
-def ocr_spell_correct_line(line):
-    return line
+Word = collections.namedtuple('Word', 'word start end is_word')
+
+
+class Words:
+
+    def __init__(self):
+        self.words = []
+        self.curr = None
+
+    def append(self, obj):
+        self.words.append(obj)
+
+    def __iter__(self):
+        for i, word in enumerate(self.words):
+            self.curr = i
+            yield word
+        self.curr = None
+
+    def next_word(self):
+        """get next word while iterating through Words object"""
+        if self.curr is None:
+            raise ValueError('No current word. Use for loop to create this context.')
+        if self.curr >= len(self.words) - 1:
+            return '<EOS>'
+        return self.words[self.curr + 1]
+
+    def prev_word(self):
+        """get previous word while iterating through Words object"""
+        if self.curr is None:
+            raise ValueError('No current word. Use for loop to create this context.')
+        if self.curr <= 0:
+            return '<BOS>'
+        return self.words[self.curr - 1]
+
+    def next_next_word(self):
+        """get next word while iterating through Words object"""
+        if self.curr is None:
+            raise ValueError('No current word. Use for loop to create this context.')
+        if self.curr >= len(self.words) - 2:
+            return '<EOS>'
+        return self.words[self.curr + 2]
+
+    def prev_prev_word(self):
+        """get previous word while iterating through Words object"""
+        if self.curr is None:
+            raise ValueError('No current word. Use for loop to create this context.')
+        if self.curr <= 0:
+            return '<BOS>'
+        return self.words[self.curr - 2]
+
+
+def ocr_spell_correct_line(lm, line, cutoff=3):
+    newline = []
+    pat = re.compile(r'[A-Za-z]+')
+    words = Words()
+    apparent_word_ctr = 0
+    for m in pat.finditer(line):
+        word = m.group()
+        apparent_word = len(word) > cutoff and word in lm
+        if apparent_word:
+            apparent_word_ctr += 1
+        words.append(Word(word, m.start(), m.end(), apparent_word))
+    idx = 0
+    for word in words:
+        if word in lm or len(word) <= cutoff:
+            newline.append(line[idx:word.end])
+            idx = word.end
+    newline.append(line[idx:])
+    return ''.join(newline)
--- tests/test_ocr.py
+++ tests/test_ocr.py
@@ -1,7 +1,26 @@
+import pytest
+
+from spellpie.lm import build_spelling_model
 from spellpie.ocr_model import ocr_spell_correct_line
 
 
-def test_garbage_pattern():
+@pytest.fixture()
+def simple_lm():
+    return build_spelling_model([
+        'immature granulocytes',
+    ])
+
+
+@pytest.mark.parametrize(('line', ), [
+    (r'(ous 0 ==> oes .+ . ale e=', )
+])
+def test_garbage_pattern(simple_lm, line):
     """These patterns should not be altered"""
-    line = r'(ous 0 ==> oes .+ . ale e='
-    assert ocr_spell_correct_line(line) == line
+    assert ocr_spell_correct_line(simple_lm, line) == line
+
+
+@pytest.mark.parametrize(('line', 'exp'), [
+    (r'immature Granuiocvtes% 0: cl e % BRH', r'immature Granulocytes% 0: cl e % BRH')
+])
+def test_good_pattern(simple_lm, line, exp):
+    assert ocr_spell_correct_line(simple_lm, line) == exp
